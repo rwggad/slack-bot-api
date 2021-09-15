25d9395804690cc9e4327c3418a48da0111a86d2
Kim Jeong Hwan <rwggad@gmail.com>
2021-09-15 17:12:17 +0900

    [timer bot] classifying functions

--- timer_bot/main.py
+++ timer_bot/main.py
@@ -9,66 +9,104 @@ CHANNEL_NAME = ""
 SLACK_BOT_NAME = ""
 
 
-def cal_total_second(t):
-    return (t.hour * 3600) + (t.minute * 60)
+class TimerBot(object):
+    HOUR_TO_SEC = 3600
+    MINUTE_TO_SEC = 60
 
+    FINISH_FMT = "{U}님 퇴근!"
+    H_M_FMT = "{U}님 퇴근까지 {H}시간 {M}분 남으셨습니다 ㅎ"
+    M_FMT = "{U}님 퇴근까지 {M}분 남으셨습니다 ㅎ"
 
-def get_usr_table():
-    usr_table = {}
-    with open('./usr_data') as f:
-        usr_list = f.readlines()
+    def __init__(self):
+        self.__usr_table = {}
+        self.__send_msg = ''
 
-        for usr in usr_list:
-            usr_elem = usr.split()
-            if len(usr_elem) != 2:
+    def __get_usr_table(self):
+        usr_table = {}
+        with open('./usr_data') as f:
+            usr_list = f.readlines()
+
+            for usr in usr_list:
+                usr_elem = usr.split()
+                if len(usr_elem) != 2:
+                    continue
+
+                name = usr_elem[0]
+                time = usr_elem[1]
+                usr_table[name] = datetime.strptime(time,"%H:%M:%S")
+
+        return usr_table
+
+    def set_usr_table(self):
+        self.__usr_table = self.__get_usr_table()
+
+    def get_usr_table(self):
+        return self.__usr_table
+
+    def __make_send_msg(self):
+        def cal_total_second(t):
+            return (
+                (t.hour * self.HOUR_TO_SEC) + (t.minute * self.MINUTE_TO_SEC)
+            )
+
+        msg_list = []
+        cur_time = datetime.now()
+        cur_time_to_sec = cal_total_second(cur_time)
+
+        for user, user_time in self.__usr_table.items():
+            user_time_to_sec = cal_total_second(user_time)
+
+            sec_interval = (user_time_to_sec - cur_time_to_sec)
+
+            if cur_time_to_sec >= user_time_to_sec:
+                msg_list.append(self.FINISH_FMT.format(U=user))
                 continue
 
-            name = usr_elem[0]
-            time = usr_elem[1]
-            usr_table[name] = datetime.strptime(time,"%H:%M:%S")
+            elif sec_interval <= self.HOUR_TO_SEC:
+                msg_list.append(
+                    self.M_FMT.format(
+                        U=user,
+                        M=str(int(sec_interval / self.MINUTE_TO_SEC))
+                    )
+                )
+                continue
 
-    return usr_table
+            else:
+                remain_hour = int(sec_interval / self.HOUR_TO_SEC)
+                remain_minute = int(
+                    (sec_interval % self.HOUR_TO_SEC) / self.MINUTE_TO_SEC
+                )
 
+                msg_list.append(
+                    self.H_M_FMT.format(
+                        U=user,
+                        H=remain_hour,
+                        M=remain_minute
+                    )
+                )
+                continue
 
-def make_send_msg(usr_table):
-    msg_list = []
-    cur_time = datetime.now()
-    cur_time_second = cal_total_second(cur_time)
+        if msg_list:
+            return '\n'.join(msg_list)
 
-    for user, user_time in usr_table.items():
-        user_time_second = cal_total_second(user_time)
+        return ''
 
-        if cur_time_second >= user_time_second:
-            msg_list.append("{U}님 퇴근!".format(U=user))
-            continue
+    def set_send_msg(self):
+        self.__send_msg = self.__make_send_msg()
 
-        # cal time interval
-        time_interval = int((user_time - cur_time).seconds / 60)
-        msg_list.append(
-            "{U}님 퇴근까지 {H}시간 {M}분 남으셨습니다 ㅎ".format(
-                U=user,
-                H=str(int(time_interval/60)),
-                M=str(int(time_interval%60))
-            )
-        )
-
-    if msg_list:
-        return '\n'.join(msg_list)
-
-    return ''
+    def get_send_msg(self):
+        return self.__send_msg
 
 
 def run():
-    usr_table = get_usr_table()
-    if not usr_table:
-        return
-
     print('running timer bot ({})'.format(datetime.now()))
 
+    tb = TimerBot()
+    tb.set_usr_table()
+    tb.set_send_msg()
+
     hook = InCommingWebHooks(INCOMING_URL, CHANNEL_NAME, SLACK_BOT_NAME)
-    msg = make_send_msg(usr_table)
-    if msg:
-        hook.send_msg(msg)
+    hook.send_msg(tb.get_send_msg())
 
 
 def main():
